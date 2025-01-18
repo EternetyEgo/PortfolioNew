e0559febca3ad85e12daced96a42aa615b118da0
ZetsuCoder <codewiizard@gmail.com>
2025-01-18 23:27:27 +0500

    Add page progress bar and scroll-to-top functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { Box } from "@chakra-ui/react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import ScrollToTop from "react-scroll-to-top";
+import PageProgressBar from "page-progressbar-react";
 
 function App() {
   return (
     <Box>
+      <ScrollToTop style={{paddingLeft: "5px"}} smooth />
+      <Box pos={"relative"} zIndex={999}><PageProgressBar /></Box>
       <Routes>
         <Route path="/" element={<Home />}></Route>
       </Routes>
