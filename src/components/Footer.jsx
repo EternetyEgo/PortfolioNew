e2db88d861626f73acc1c019b7e4927e66632501
ZetsuCoder <codewiizard@gmail.com>
2025-01-19 00:11:58 +0500

    sas

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -18,9 +18,9 @@ export default function Footer() {
       </Text>
 
       <Flex gap="25px" mb={6}>
-        <FaInstagram  cursor={"pointer"} size={35} />
-        <FaTelegram cursor={"pointer"} size={35} />
-        <FaGithub cursor={"pointer"} size={35} />
+        <FaInstagram  className="icon" cursor={"pointer"} size={35} />
+        <FaTelegram className="icon" cursor={"pointer"} size={35} />
+        <FaGithub className="icon" cursor={"pointer"} size={35} />
       </Flex>
     </Box>
   );
